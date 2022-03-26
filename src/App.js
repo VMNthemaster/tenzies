c8de8bab875ea11e2fae16a7cd993ccaa0b1c67f
VMNthemaster <varad.nadkarni09@gmail.com>
2022-03-26 21:23:42 +0530

    responsive improvements

--- src/App.js
+++ src/App.js
@@ -100,23 +100,23 @@ function App() {
   return (
     <>
       <main className="">
-        <div className="h-[90vh] mx-[20%] my-[5vh] bg-gray-100  rounded-lg flex flex-col justify-around">
+        <div className="h-[90vh] md:mx-[20%] my-[5vh] bg-gray-100  rounded-lg flex flex-col justify-around">
           {tenzies && <Confetti />}
-          <h1 className="text-center text-[5vw] text-red-600 italic">Tenzies</h1>
+          <h1 className="text-center text-[10vw] md:text-[5vw] text-red-600 italic">Tenzies</h1>
 
           <div className='w-[50%] items-center mx-auto bg-violet-500 rounded-md  shadow-md'>
-          {tenzies && <h1 className='text-center font-[gabriola] text-[2vw] z-10 h-[10vh] text-yellow-200 animation'>Congratulations!!!</h1>}
-          {tenzies && <h1 className='text-center text-white font-[ronnia] text-[2vw] z-10'>Rolls Required: {rollsRequired}</h1>}
-          {tenzies && <h1 className='text-center text-white font-[ronnia] text-[2vw] z-10'>Time Taken : {timeTaken.endTime/1000} sec</h1>}
+          {tenzies && <h1 className='text-center font-[gabriola] text-[5vw] md:text-[2vw] z-10 h-[10vh] text-yellow-200 animation'>Congratulations!!!</h1>}
+          {tenzies && <h1 className='text-center text-white font-[ronnia] animation2 text-[2vw] z-10'>Rolls Required: {rollsRequired}</h1>}
+          {tenzies && <h1 className='text-center text-white font-[ronnia] animation2 text-[2vw] z-10'>Time Taken : {timeTaken.endTime/1000} sec</h1>}
           </div>
 
-          <p className="text-center text-[2vw]">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+          <p className="text-center text-[5vw] md:text-[2vw] p-1">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
           <div className="grid grid-cols-5  p-4 gap-4">
             {diceElements}
           </div>
 
 
-          <button onClick={rollDice} className='w-[20%] h-[7%] border-2 mx-auto text-3xl  font-[gabriola] bg-blue-600 text-white rounded-md hover:bg-blue-500'>{tenzies ? "New Game" : "ROLL"}</button>
+          <button onClick={rollDice} className='w-[20%] h-[7%] border-2 mx-auto text-[5vw] md:text-[2vw] font-[gabriola] bg-blue-600 text-white rounded-md hover:bg-blue-500'>{tenzies ? "New Game" : "ROLL"}</button>
         </div>
       </main>
     </>
